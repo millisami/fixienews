feb3225fa0aef3359255d50c03bc02897a39eed7
Joe Van Dyk <joe@fixieconsulting.com>
2009-11-07 16:51:54 -0800

    Update

--- news.rb
+++ news.rb
@@ -1,6 +1,57 @@
 require 'rubygems'
 require 'sinatra'
+require 'open-uri'
+require 'nokogiri'
 
-get '/' do
-  "Hello!"
+Y = "http://news.ycombinator.com"
+
+def url_for_y path
+  "#{Y}#{path}"
+end
+
+def cache_it
+  headers['Cache-Control'] = 'public, max-age=60'
+end
+
+def remote_load url
+  open(url).read
+end
+
+def style_for_iphone text
+  doc = Nokogiri::HTML(text)
+
+  html =  '<html><head>'
+  html << '<meta name="viewport" content="width=320; initial-scale=0.6; maximum-scale=1.0; user-scalable=0;"/>'
+  html << '<style type="text/css" media="screen">@import "/iphonenav.css";</style>'
+  html << '<title>Fixie Iphone for y-combinator</title>'
+  html << '</head>'
+  html << '<body>'
+
+  html << '<h1 id="pageTitle"><a href="/">Y Combinator</a></h1>'
+
+  html << '<ul selected="true">'
+  doc.css('tr').each do |row|
+    if story_title = row.css('td.title > a').first
+      html << '<li>'
+      html << "<a href='#{story_title['href']}'>#{story_title.content}</a>"
+    end
+    if subtext = row.css('td.subtext').first
+      comments_link = subtext.css('a').last
+      html << "<a href='#{comments_link['href']}'>#{comments_link.content}\n"
+      html << '</li>'
+    end
+  end
+  html << '</ul>'
+  html << '</body></html>'
+  html
+end
+
+get '*' do
+  cache_it
+  content = remote_load(url_for_y(request.fullpath))
+  if %w(/ /x).include?(request.path_info)
+    style_for_iphone(content)
+  else
+    content
+  end
 end
